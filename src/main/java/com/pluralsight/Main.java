ce0fa7236b1722b0f513e38dbaa1350ff5835a14
RickelviC <Cruzrickelvi@gmail.com>
2026-04-21 11:41:56 -0400

    added user input for what files to read and what file to write in. also closed the new bufferedwriter and scanner

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,31 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         try {
-            FileReader fileReader = new FileReader("employees.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            System.out.print("what file do you want to read from? (employees.csv): ");
+            String userInputRead = scanner.nextLine();
+
+            System.out.print("what file do you want to write in?: ");
+            String userInputWrite = scanner.nextLine();
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(userInputRead));
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(userInputWrite));
 
             String line;
+            String text;
 
             while ((line = bufferedReader.readLine()) != null) {
 
@@ -24,19 +38,20 @@ public class Main {
                 double hours = Double.parseDouble(myArray[2].trim());
                 double pay = Double.parseDouble(myArray[3].trim());
 
-                Employee employee = new Employee(id,name,hours,pay);
+                Employee employee = new Employee(id, name, hours, pay);
 
-                System.out.printf("Employee ID: %d, Name: %s, Gross pay: $%.2f%n",
-                        employee.getEmployeeId(),employee.getName(),employee.getGrossPay());
+                text = String.format("Employee ID: %d, Name: %s, Gross pay: $%.2f%n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
 
-                //System.out.printf(employee.getEmployeeId()+ ": " + employee.getName() + " Made $" + employee.getGrossPay()+"\n");
+                bufferedWriter.write(text);
 
             }
 
             bufferedReader.close();
+            bufferedWriter.close();
 
         } catch (Exception ex) {
             System.err.println("Something went wrong reading the files!");
         }
+        scanner.close();
     }
 }
